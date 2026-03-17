7cb8682845e40064c5b7c0c922df4666a99ddc1c
sahilcodexx <sahil171759@gmail.com>
2026-03-18 00:47:38 +0530

    feat: add gradient fade to bottom of page

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,7 @@ function App() {
             <Footer />
           </Layout>
         </Container>
+        <div className="from-background pointer-events-none fixed inset-x-0 bottom-0 z-40 h-10 bg-linear-to-t to-transparent [mask-image:linear-gradient(to_top,black_10%,transparent)] opacity-100 backdrop-blur-[5px] select-none [-webkit-mask-image:linear-gradient(to_top,black_50%,transparent)] dark:[mask-image:linear-gradient(to_top,black_20%,transparent)] dark:[-webkit-mask-image:linear-gradient(to_top,black_40%,transparent)]" />
       </div>
     </ThemeProvider>
   );
