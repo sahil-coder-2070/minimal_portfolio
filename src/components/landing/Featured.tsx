99d650b09566ef5526131c0063595392cdfaffba
sahilcodexx <sahil171759@gmail.com>
2026-08-02 02:02:57 +0530

    refactor(landing): replace old GitHub calendar with new GitHubActivity component

--- src/components/landing/Featured.tsx
+++ src/components/landing/Featured.tsx
@@ -3,37 +3,21 @@
 import React from 'react';
 import Container from '../layouts/Container';
 import SectionHeading from '../common/SectionHeading';
-import { GitHubCalendar } from 'react-github-calendar';
-import { useTheme } from './theme-provider';
 import RepeatSeparator from '../ui/repeat-separator';
+import { GitHubActivity } from '../ui/github-activity';
 
-const Featured = ({ contributions = 0 }: { contributions?: number }) => {
-  const { theme } = useTheme();
-  const currentTheme = theme;
-
+const Featured = () => {
   return (
     <>
       <RepeatSeparator />
       <SectionHeading heading={'GitHub Activity'} />
       <Container className="py-4">
-        <div className="space-y-6 px-4 py-1">
-          <div className="flex w-full justify-between">
-            <p className="text-primary mt-1 text-sm font-medium">
-              Total: <span className="font-bold">{contributions} </span> contributions
-            </p>
-          </div>
-          <div className="inset-shadow bg-background/50 github-card-shadow relative flex items-center justify-center rounded-lg border border-black/10 p-2 px-5 py-2 backdrop-blur-sm sm:p-4 md:p-6 dark:border-white/10">
-            <GitHubCalendar
-              username="sahilcodexx"
-              blockSize={7}
-              blockMargin={5}
-              colorScheme={currentTheme === 'dark' ? 'dark' : 'light'}
-              theme={{
-                light: ['#ebedf0', '#c6cbd1', '#8b949e', '#57606a', '#24292f'],
-                dark: ['#161b22', '#2d333b', '#636e7b', '#8b949e', '#c9d1d9'],
-              }}
-            />
-          </div>
+        <div className="flex justify-center px-4 py-1">
+          <GitHubActivity
+            username="sahilcodexx"
+            showMonths={true}
+            defaultOpen={false}
+          />
         </div>
       </Container>
     </>
